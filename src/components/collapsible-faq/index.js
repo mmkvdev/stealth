3416be8d9f6a4bb4b062e294b301dd61730b2e8a
mmkvdev <madhuv299@gmail.com>
2024-09-21 18:43:19 +0530

    code: test changes

--- src/components/collapsible-faq/index.js
+++ src/components/collapsible-faq/index.js
@@ -1,51 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import faqList from "../../data/faqList";
 
 const classes = "flex justify-content-between align-items-center";
 
 function Faqs() {
-  return (
-    <>
-      <div className="flex justify-content-center mt-75 w-100 h-50">
-        <div className="w-50" data-testid="faq-list">
-          <div className="item my-10">
-            <div className={`ques ${classes} pa-15 mb-3`}>
-              <h4 className="flex align-items-center my-2">
-                What is HackerRank?
-              </h4>
-              <span>-</span>
-            </div>
-            <div className={`closed open ${classes}`}>
-              <p className="my-0">
-                HackerRank is a place where programmers from all over the world
-                come together to solve problems in a wide range of Computer
-                Science domains such as algorithms, machine learning, or
-                artificial intelligence, as well as to practice different
-                programming paradigms like functional programming.
-              </p>
-              <div className={classes}>
-                <button className="danger small">Delete</button>
-              </div>
-            </div>
-          </div>
-          <div className={classes}>
+  const [faqItems, setFAQItems] = useState(faqList);
+  const [openQuestionItemIndex, setOpenQuestionItemIndex] = useState(null);
+  const [newFAQItem, setNewFAQItem] = useState({
+    question: "",
+    answer: "",
+  });
+
+  const handleAnswerVisibility = (answerItemIndex) => {
+    setOpenQuestionItemIndex(
+      openQuestionItemIndex === answerItemIndex ? null : answerItemIndex
+    );
+  };
+
+  const getAnswerItemClassName = (index) => {
+    return openQuestionItemIndex === index ? "open" : "closed";
+  };
+
+  const expandQuestion = (index) =>
+    openQuestionItemIndex === index ? "-" : "+";
+
+  const deleteQuestion = (questionIndex) => {
+    setFAQItems(faqItems.filter((_, i) => i !== questionIndex));
+  };
+
+  const renderFAQListItems = (listItem, listIndex) => (
+    <div className="item my-10" key={listIndex}>
+      <div
+        className={`ques ${classes} pa-15 mb-3`}
+        onClick={() => handleAnswerVisibility(listIndex)}
+      >
+        <h4 className="flex align-items-center my-2">{listItem.question}</h4>
+        <span>{expandQuestion(listIndex)}</span>
+      </div>
+      <div className={`${getAnswerItemClassName(listIndex)} ${classes}`}>
+        <p className="my-0">{listItem.answer}</p>
+        <div className={classes} onClick={() => deleteQuestion(listIndex)}>
+          <button className="danger small">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+
+  const renderNewFAQItem = () => (
+    <div className={classes}>
             <div>
               <textarea
                 className="ma-8 w-100"
                 placeholder="Enter the question"
                 data-testid="add-question"
-                value=""
+                onChange={(e) => handleNewFAQItemChange(e, 'question')}
+                value={newFAQItem.question}
               />
               <textarea
                 className="ma-8 w-100"
                 placeholder="Enter the answer"
                 data-testid="add-answer"
-                value=""
+                onChange={(e) => handleNewFAQItemChange(e, 'answer')}
+                value={newFAQItem.answer}
               />
             </div>
-            <button data-testid="add-faq-button">Add</button>
+            <button data-testid="add-faq-button" onClick={addFAQ}>Add</button>
           </div>
+  )
+
+  const addFAQ = () => {
+    if (!newFAQItem.question || !newFAQItem.answer) {
+      alert("Please add both question and answer");
+      return;
+    }
+
+    setFAQItems([
+      ...faqItems,
+      {
+        ...newFAQItem,
+      },
+    ]);
+    setNewFAQItem({
+      question: '',
+      answer: ''
+    });
+    setOpenQuestionItemIndex(null);
+  };
+
+  const handleNewFAQItemChange = (userInput, faqType) => {
+    const { value } = userInput.target;
+    setNewFAQItem({
+      ...newFAQItem,
+      [faqType]: value
+    })
+  };
+
+  return (
+    <>
+      <div className="flex justify-content-center mt-75 w-100 h-50">
+        <div className="w-50" data-testid="faq-list">
+          {faqItems.map((listItem, listIndex) =>
+            renderFAQListItems(listItem, listIndex)
+          )}
+          {renderNewFAQItem()}
         </div>
       </div>
     </>
